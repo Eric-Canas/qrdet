77c2bdfce9ea01aac4f109f68aa9db1c1fbaa087
Eric-Canas <elcorreodeharu@gmail.com>
2023-01-17 09:40:29 +0100

    More readable image

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # Draw the detections
     for (x1, y1, x2, y2), confidence in detections:
         cv2.rectangle(image, (x1, y1), (x2, y2), color=(0, 255, 0), thickness=2)
-        cv2.putText(image, f'{confidence:.2f}', (x1, y1 - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5,
+        cv2.putText(image, f'{confidence:.2f}', (x1, y1 - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
                     color=(0, 255, 0), thickness=2)
     # Save the results
     cv2.imwrite(filename='resources/qreader_test_image_detections.jpeg', img=image)
